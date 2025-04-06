1d892cdf49aeb92d8a8095e080a4840638355fe7
7sod <151560958+7sod@users.noreply.github.com>
2025-04-06 06:02:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import shutil
+import os
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+from ai import run_model
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message":"API is live"}
+
+@app.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    try:
+        temp_video_path = f"temp_{file.filename}"
+        with open(temp_video_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor() as pool:
+            result = await loop.run_in_executor(pool, run_model, temp_video_path)
+
+        os.remove(temp_video_path)
+        return JSONResponse(content={"result": result})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
