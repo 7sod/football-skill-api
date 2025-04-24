9495e60ba3c5c50b2432da5ccf0f14cdc470f8d6
7sod <151560958+7sod@users.noreply.github.com>
2025-04-24 22:47:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, UploadFile, File , Form
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import shutil
+import os
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+from ai import run_model
+from database import create_tables
+
+app = FastAPI()
+
+# تشغيل إنشاء الجداول مرة واحدة
+create_tables()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://v0-score-tech.vercel.app/performance-analysis"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message":"API is live"}
+
+@app.post("/analyze")
+async def analyze(
+    file: UploadFile = File(...),
+    full_name: str = Form(...),
+    weight: float = Form(...),
+    height: float = Form(...),
+    diet: str = Form(...),
+    training: str = Form(...),
+    position: str = Form(...)
+):
+    try:
+        temp_video_path = f"temp_{file.filename}"
+        with open(temp_video_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        loop = asyncio.get_event_loop()
+        with ThreadPoolExecutor() as pool:
+            result = await loop.run_in_executor(pool, lambda: run_model(
+                temp_video_path,
+                full_name=full_name,
+                weight=weight,
+                height=height,
+                diet=diet,
+                training=training,
+                position=position
+            ))
+
+        os.remove(temp_video_path)
+        return JSONResponse(content={"result": result})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
